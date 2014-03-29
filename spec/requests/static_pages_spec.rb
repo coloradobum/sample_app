7f3a00b8889341535d4b085cfc64235ce8807f04
Jeff Thomas <subskriptions@gmail.com>
2014-03-29 12:52:05 -0600

    Added additional example.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,5 +42,10 @@ describe "Static pages" do
       visit '/static_pages/contact'
       expect(page).to have_content('Contact')
     end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
   end
 end
