b79ddedbec080bd96475ba2f12a5c72ab6b43a8c
Jeff Thomas <subskriptions@gmail.com>
2014-03-29 11:50:08 -0600

    Failing tests for dynamic page titles.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,9 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -16,6 +16,11 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe "About page" do
@@ -24,5 +29,10 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      end
   end
 end
